cbb1decb759f013f5a7420462511eff4363d73e0
Eoo0m <jseom818@gmail.com>
2025-04-16 17:06:16 +0900

    Update denoise.py

--- denoise/denoise.py
+++ denoise/denoise.py
@@ -72,3 +72,15 @@ def median_filter(img, kernel_size):
                 window = padded_img[y:y + kernel_size, x:x + kernel_size, c]
                 output[y,x,c] = np.median(window)
     return output
+
+def calculate_rmse(img1, img2):
+    """
+    Calculates RMS error between two images. Two images should have same sizes.
+    """
+    if (img1.shape[0] != img2.shape[0]) or \
+            (img1.shape[1] != img2.shape[1]) or \
+            (img1.shape[2] != img2.shape[2]):
+        raise Exception("img1 and img2 should have sime sizes.")
+
+    diff = np.abs(img1.astype(dtype=int) - img2.astype(dtype=int))
+    return np.sqrt(np.mean(diff ** 2))
